c7cbea2590e58559b0ca31b0943b07be80735c66
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2023-11-09 00:18:14 +0530

    Update law_chatbot.py

--- law_chatbot.py
+++ law_chatbot.py
@@ -20,11 +20,11 @@ st.sidebar.title("Customization Options")
     
 customization_options = {
         "country": st.sidebar.selectbox("Select Country", ["INDIA", "USA", "UK"]),
+        "web": st.sidebar.radio("Search the Web?", ["No", "Yes"]),
+        "info":st.sidebar.info('Select Yes to find content from the internet', icon="ℹ️"),
         "use_chatgpt":st.sidebar.radio("Use ChatGPT", ["No", "Yes"]),
         "words": st.sidebar.slider("Number of words", 0, 750, 50),
         "src": st.sidebar.radio("Show Source of Text", ["No", "Yes"]),
-        "web": st.sidebar.radio("Search the Web?", ["No", "Yes"]),
-        "info":st.sidebar.info('Select Yes to find content from the internet', icon="ℹ️"),
     }
 
 country_cid = {"INDIA": 5, "USA": 6, "UK": 7}
